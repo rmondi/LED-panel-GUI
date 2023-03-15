5fd8e2c22c8e73fa7f03e774f5b0c843d9bc8c4f
Rémy Mondi <remy@mondi.dev>
2023-03-15 16:57:40 +0100

    [Update] React render method

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 
 import "./modules/vendors/reset-css/reset-css.scss"
 import App from "./modules/App/App.js"
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-);
+const container = document.getElementById('app')
+const root = createRoot(container)
+
+root.render(<App />);
